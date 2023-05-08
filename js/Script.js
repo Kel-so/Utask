599ee4868963ebc6cd28dba08ffada541f7b1e48
Kelson <kelson.felp@gmail.com>
2023-05-07 23:51:19 -0300

    1.5

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,5 @@
+// Modal
+
 const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
@@ -9,4 +11,45 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
   modal_container.classList.remove('show')
 })
+
+// To-do aplication
+
+const form = document.querySelector('form');
+const todoList = document.getElementById('todo-list');
+const ongoingList = document.getElementById('ongoing-list');
+const completedList = document.getElementById('completed-list');
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+
+const name = document.getElementById('project-name').value;
+const description = document.getElementById('project-description').value;
+const newItem = document.createElement('li');
+newItem.textContent = `${name} - ${description}`;
+
+
+todoList.appendChild(newItem);
+form.reset();
+});
+
+
+todoList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    ongoingList.appendChild(event.target);
+  }
+});
+
+ongoingList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    completedList.appendChild(event.target);
+  }
+});
+
+completedList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+
+    event.target.remove();
+  }
+});
 
